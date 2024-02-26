165761c9b61483c316a2ad1d4b8acb3f4cd660c7
Jamie Reason <95385925+jamiereason@users.noreply.github.com>
2024-02-26 15:39:26 +0000

    added simple 2d BL emulator and example

---
+++ src/2d-simple-ble.R
@@ -0,0 +1,87 @@
+library(viridisLite)
+
+# Simple BL Emulator
+source("src/lib/simple_BL_emulator.R")
+
+
+### Plotting setup
+axis_labels <- c("", "")  #c(expression(x), expression(y))
+points_plot <- function(xD, col="black", cex=1.5, pch=20, ...) points(xD, col=col, cex=cex, pch=pch, ...)
+
+var_cols <- function(n) hcl.colors(n, "YlOrRd", rev=TRUE)
+surf_cols <- magma
+diag_cols <- turbo
+
+
+### True Function
+f <- function(X){
+  A <- exp(-10*((X[,1]-0.4)^2 + (X[,2]-0.7)^2))
+  B <- exp(-20*((X[,1]-0.9)^2 + (X[,2]-0.5)^2))
+  C <- cos(6*(X[,1]^2 - X[,2]^2))
+  return(A + 5/3 * B + 1/3 * C - 0.5)
+}
+
+
+### Plotting
+n_seq <- 101
+x_seq <- seq(0,1, len=n_seq)
+xP <- as.matrix(expand.grid(x_seq, x_seq))
+
+# Evaluate true function
+fP <- f(xP)
+fP_mat <- matrix(fP, nrow=n_seq, ncol=n_seq)  # resize to matrix for plotting
+
+# Plot true function
+pdf(file="figures/2d-simple-ble.pdf", width=7, height=6)
+par(mar=c(4,4,2,4))
+filled.contour(x=x_seq, y=x_seq, z=fP_mat, color.palette=surf_cols, levels=seq(-1, 1, 0.1),
+               xlab=axis_labels[1], ylab=axis_labels[2],
+               plot.axes={axis(1);axis(2)
+                 contour(x_seq, x_seq, fP_mat, add=TRUE, level=seq(-1, 1, 0.1), lwd=0.4, drawlabels=FALSE)
+               })
+
+
+nD_seq <- 4
+xD_seq <- seq(0.05, 0.95, len=nD_seq)
+
+xD <- as.matrix(expand.grid(xD_seq, xD_seq))
+fD <- f(xD)
+
+# Simple 2D BL emulator
+BL_em <- simple_BL_emulator(xD=xD, D=fD, xP=xP, theta=0.35, sig=1, mu=0)
+exp_mat <- matrix(BL_em[["ExpD_f(x)"]], nrow=n_seq, ncol=n_seq)
+var_mat <- matrix(BL_em[["VarD_f(x)"]], nrow=n_seq, ncol=n_seq)
+
+
+# Emulator expectation plot
+filled.contour(x=x_seq, y=x_seq, z=exp_mat, color.palette=surf_cols, level=seq(-1, 1, 0.1),
+               xlab=axis_labels[1], ylab=axis_labels[2],
+               plot.axes={axis(1);axis(2)
+                 contour(x_seq, x_seq, exp_mat, add=TRUE, level=seq(-1, 1, 0.1), lwd=0.4, drawlabels=FALSE)
+                 points_plot(xD)
+               })
+
+# Emulator variance plot
+filled.contour(x=x_seq, y=x_seq, z=var_mat, color.palette=var_cols, levels=seq(0,0.1,0.01),
+               xlab=axis_labels[1], ylab=axis_labels[2],
+               plot.axes={axis(1);axis(2)
+                 points_plot(xD)
+               })
+
+# Emulator sd plot
+sd_mat <- sqrt(var_mat)
+filled.contour(x=x_seq, y=x_seq, z=sd_mat, color.palette=var_cols, levels=seq(0,0.3,0.025),
+               xlab=axis_labels[1], ylab=axis_labels[2],
+               plot.axes={axis(1);axis(2)
+                 points_plot(xD)
+               })
+
+# Emulator diagnostics plot
+diag_mat <- (exp_mat - fP_mat) / sd_mat
+filled.contour(x=x_seq, y=x_seq, z=diag_mat, color.palette=diag_cols, levels=seq(-3,3,0.25),
+               xlab=axis_labels[1], ylab=axis_labels[2],
+               plot.axes={axis(1);axis(2)
+                 contour(x_seq, x_seq, diag_mat, add=TRUE, level=seq(-3, 3, 0.25), lwd=0.4, drawlabels=FALSE)
+                 points_plot(xD)
+               })
+dev.off()
---
+++ src/lib/simple_BL_emulator.R
@@ -0,0 +1,36 @@
+### Simple Bayes Linear Emulator
+# uses isotropic squared exponential covariance structure
+
+library(pdist)
+
+simple_BL_emulator <- function(xD, D, xP, theta=1, sig=1, nugget=0.00001, mu=0, just_var=TRUE){
+  n <- length(D); nP <- nrow(xP)
+  xD <- t(t(xD)/theta); xP <- t(t(xP)/theta)
+  
+  Cmat <- function(dist_mat) sig^2 * exp(-(dist_mat)^2) * (1+nugget)
+  
+  E_D <- rep(mu, n)
+  
+  Var_D <- Cmat(as.matrix(dist(xD)))
+  Var_D <- Var_D + nugget*diag(nrow(Var_D)) # for stability
+  E_B <- rep(mu, nP)
+  Cov_BD <- Cmat(as.matrix(pdist(xP, xD)))
+  Var_D_inv <- chol2inv(chol(Var_D))
+  
+  # Perform Bayes Linear adjustment to find Adjusted Expectation and Variance of f(x)
+  Cov_BD_Var_D_inv <- Cov_BD %*% Var_D_inv # need twice, so precalculate
+  ED_B <- E_B + Cov_BD_Var_D_inv %*% (D - E_D)
+  if (just_var){
+    diag_Var_B <- rep(sig^2, nP)
+    
+    diag_VarD_B <- diag_Var_B - apply(Cov_BD_Var_D_inv * Cov_BD, 1, sum)
+    VarD_B <- diag_VarD_B  # return diagonal only
+  } else {
+    Var_B <- Cmat(as.matrix(dist(xP)))
+    Var_B <- Var_B + nugget*diag(nrow(Var_B)) # for stability
+    
+    VarD_B <- Var_B - Cov_BD_Var_D_inv %*% t(Cov_BD)
+  }
+  
+  return(list("ExpD_f(x)"=ED_B, "VarD_f(x)"=VarD_B))
+}
