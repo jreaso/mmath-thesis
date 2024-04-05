bbea9760148a01317a6bc973c7520d58ed84ac9f
Jamie Reason <95385925+jamiereason@users.noreply.github.com>
2024-04-05 23:00:02 +0100

    modifies plots to use grid design and different theta

--- src/robot-arm.R
+++ src/robot-arm.R
@@ -7,7 +7,7 @@ source("src/models/robot.R")
 source("src/lib/simple_BL_emulator.R")
 
 # Maximin LHD (2D)
-source("src/lib/maximin_lhd_2d.R")
+#source("src/lib/maximin_lhd_2d.R")
 
 # Matrix wrapper for `robot` function
 f <- function(X) {
@@ -42,7 +42,7 @@ fP_mat <- matrix(fP, nrow=ntheta, ncol=ntheta)
 tick_vals <- c(0, pi/2, pi, 3*pi/2, 2*pi)
 tick_labs <- expression(0, pi/2, pi, 3*pi/2, 2*pi)
 
-
+pdf(file="figures/2d-robot-arm-naive.pdf", width=7, height=6)
 ### True Function
 filled.contour(x=theta_seq, y=theta_seq, z=fP_mat, color.palette=surf_cols, levels=seq(0, 3, 0.2),
                xlab=expression(theta[2]), ylab=expression(theta[4]),
@@ -55,8 +55,11 @@ filled.contour(x=theta_seq, y=theta_seq, z=fP_mat, color.palette=surf_cols, leve
 
 
 ### Naive emulation without exploiting periodicity
-set.seed(6)
-xD <- maximin_lhd_2d(32, perturb=F)*2*pi
+#set.seed(6)
+#xD <- maximin_lhd_2d(20, perturb=F)*2*pi
+nD_seq <- 5
+tD_seq <- seq(pi/nD_seq, 2*pi - pi/nD_seq, l=nD_seq)
+xD <- expand.grid(tD_seq, tD_seq)
 nD <- nrow(xD)
 xD_all <- cbind(rep(0, nD), xD[, 1], rep(pi/6, nD), xD[, 2],
                 rep(0.8, nD), rep(0.4, nD), rep(0.6, nD), rep(1, nD))
@@ -65,7 +68,7 @@ fD <- f(xD_all)
 
 
 # Simple 2D BL emulator
-BL_em <- simple_BL_emulator(xD=xD, D=fD, xP=xP, theta=1.6, sig=1, mu=0)
+BL_em <- simple_BL_emulator(xD=xD, D=fD, xP=xP, theta=2, sig=3, mu=1.5)
 exp_mat <- matrix(BL_em[["ExpD_f(x)"]], nrow=ntheta, ncol=ntheta)
 var_mat <- abs(matrix(BL_em[["VarD_f(x)"]], nrow=ntheta, ncol=ntheta))  # abs taken to correct for numerical instability
 
@@ -91,9 +94,8 @@ filled.contour(x=theta_seq, y=theta_seq, z=exp_mat, color.palette=surf_cols, lev
                          lwd=0.4, drawlabels=FALSE)
                })
 
-# Emulator variance plot
-#sd_mat <- sqrt(abs(var_mat))
-filled.contour(x=theta_seq, y=theta_seq, z=var_mat, color.palette=var_cols, levels=seq(0, 0.1, 0.01),
+# Emulator sd plot
+filled.contour(x=theta_seq, y=theta_seq, z=sqrt(var_mat), color.palette=var_cols, levels=seq(0, 1, 0.1),
                xlab=expression(theta[2]), ylab=expression(theta[4]),
                plot.axes={
                  axis(1, at=tick_vals, labels=tick_labs, las=1)
@@ -110,6 +112,7 @@ filled.contour(x=theta_seq, y=theta_seq, z=diag_mat, color.palette=diag_cols, le
                          lwd=0.4, drawlabels=FALSE)
                  points_plot(xD)
                })
+dev.off()
 
 
 
@@ -129,7 +132,9 @@ fP_mat <- matrix(fP, nrow=ntheta, ncol=ntheta)
 tick_vals <- c(0, 2*pi)
 tick_labs <- expression(0, 2*pi)
 
+
 ### True function with extended ranges
+pdf(file="figures/2d-robot-arm-periodicity.pdf", width=7, height=6)
 filled.contour(x=theta_seq, y=theta_seq, z=fP_mat, color.palette=surf_cols, levels=seq(0, 2.8, 0.1),
                xlab=expression(theta[2]), ylab=expression(theta[4]),
                plot.axes={
@@ -145,4 +150,5 @@ filled.contour(x=theta_seq, y=theta_seq, z=fP_mat, color.palette=surf_cols, leve
                  lines(c(0, 0),       c(0, 2*pi),    lwd=4)
                  lines(c(2*pi, 2*pi), c(0, 2*pi),    lwd=4)
                })
+dev.off()
 
