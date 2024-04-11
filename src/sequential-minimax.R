abf5011555346b45be2e21b3a9920c6b437d132c
Jamie Reason <95385925+jamiereason@users.noreply.github.com>
2024-04-11 23:45:45 +0100

    seperated core sequential_minimax function to be reused

--- src/sequential-minimax.R
+++ src/sequential-minimax.R
@@ -56,31 +56,7 @@ points(xD, pch=16,col="red",cex=0.8)
 
 
 # Choosing k best points to add but from a random sample
-sequential_minimax <- function(X, k, p=3, niter=100) {
-  if (k > nrow(X)) {
-    stop("k cannot be larger than the total number of points")
-  }
-  
-  XDi <- c() # Design matrix indices
-  
-  max_dist <- max(as.matrix(dist(X)))
-  
-  while(length(XDi) < k) {
-    P <- min(p, k-length(XDi)) # number of points to add to XD this iteration
-    
-    for (i in 1:niter) {
-      new_inds <- sample(setdiff(1:nrow(X), XDi), P, replace=F) # generate a sample of P random points not in XD
-      XDj <- c(XDi, new_inds) # proposal for new design
-      new_max_dist <- max(apply(as.matrix(pdist(X[setdiff(1:nrow(X), XDj),], X[XDj,])), 1, min))
-      if(new_max_dist < max_dist){
-        max_dist <- new_max_dist
-        inds <- new_inds
-      }
-    }
-    XDi <- c(XDi, inds)
-  }
-  return(XDi)
-}
+source("src/lib/sequential_minimax.R") # moved to lib for reusability
 
 
 k <- 15; p <- 3
