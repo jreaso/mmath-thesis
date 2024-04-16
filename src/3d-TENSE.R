63f2111f1eae2ef281efcf4464362ba632fe9329
Jamie Reason <95385925+jamiereason@users.noreply.github.com>
2024-04-16 10:52:42 +0100

    refactor 3d-TENSE

--- src/3d-TENSE.R
+++ src/3d-TENSE.R
@@ -1,14 +1,21 @@
 library(viridisLite)
 library(plotly)
 
-source("src/lib/filled.contour3.R")
 source("src/lib/TENSE.R")
+source("src/lib/advanced_NS_emulator.R")
+source("src/lib/filled.contour3.R")
+source("src/lib/simple_NS_emulator2.R")
 
-###====================================
+var_cols <- function(n) hcl.colors(n, "YlOrRd", rev = TRUE)
+diag_cols <- turbo
+surf_cols <- magma #plasma
+
+
+###=========================================
 ### Embedding Surface
-###====================================
+###=========================================
 
-# Compact domain sigmoid function on [0, 1]
+# Sigmoid like function (derivative has a compact domain on [0, 1])
 s <- function(d) {
   (2*d^2 * (0 <= d & d < 0.5)) + ((1 - 2*(d-1)^2) * (0.5 <= d & d < 1)) + (1*(1 <= d))
 }
@@ -26,55 +33,74 @@ v <- function(X){
 # Embedding function
 V <- function(X) cbind(X, v(X))
 
+### Derivatives
+s_deriv <- function(d) {
+  (4*d * (0 <= d & d < 0.5)) + (-4*(d-1) * (0.5 <= d & d < 1))
+}
+
+v_deriv <- function(X){
+  if (is.matrix(X)) { x <- X[,1]; y <- X[,2]; z <- X[,3] }
+  else { x <- X[1]; y <- X[2]; z <- X[3] }
+  
+  #A <- 1 - s(3*sqrt((x-0.5)^2 + (0.5*z-0)^2 + 1e-6))
+  dxA <- -s_deriv(3*sqrt((x-0.5)^2 + (0.5*z-0)^2)) * 3/(2*sqrt((x-0.5)^2 + (0.5*z-0)^2 + 1e-6)) * 2*(x-0.5)
+  dzA <- -s_deriv(3*sqrt((x-0.5)^2 + (0.5*z-0)^2)) * 3/(2*sqrt((x-0.5)^2 + (0.5*z-0)^2 + 1e-6)) * 0.5*z
+  
+  B <- 1 - 2*(y > 0.5)
+  
+  dx <- dxA*B
+  dz <- dzA*B
+  return(cbind(dx, 0, dz))
+}
 
 
-###====================================
 ### Plotting Embedding Surface
-###====================================
+
+# Compact domain sigmoid plot
+plot(seq(-0.5, 1.5, l=100), s(seq(-0.5, 1.5, l=100)), type="l", xlab="d", ylab="s(d)", xlim=c(-0.1,1.1))
+
+
 n_seq <- 201
 x_seq <- seq(0,1, l=n_seq);
-x2_grid <- as.matrix(expand.grid(x_seq, x_seq))
+x_grid <- as.matrix(expand.grid(x_seq, x_seq))
 
-#pdf(file="figures/T3D-....pdf", width=7, height=6)
-xP1 <- cbind(x2_grid[,1], 0.25, x2_grid[,2]); vP1 <- v(xP1)
-vP1_mat <- matrix(vP1, nrow=n_seq, ncol=n_seq)
+# Slice (a)
+xP <- cbind(x_grid[,1], 0.8, x2_grid[,2])
+vP <- v(xP); vP_mat <- matrix(vP, nrow=n_seq, ncol=n_seq)
+png(file="figures/TENSE-3d/v-a.png", type="cairo", width=2400, height=2400, res=600, bg="transparent")
 par(mar=c(4,4,2,2))
-filled.contour3(x=x_seq, y=x_seq, z=vP1_mat, color.palette=magma, levels=seq(-1, 1, 0.05), xlab="x", ylab="z")
+filled.contour3(x=x_seq, y=x_seq, z=vP_mat, color.palette=magma, levels=seq(-1, 1, 0.05), xlab="x", ylab="z")
+dev.off()
 
-xP2 <- cbind(x2_grid[,1], 0.75, x2_grid[,2]); vP2 <- v(xP2)
-vP2_mat <- matrix(vP2, nrow=n_seq, ncol=n_seq)
-filled.contour3(x=x_seq, y=x_seq, z=vP2_mat, color.palette=magma, levels=seq(-1, 1, 0.05), xlab="x", ylab="z")
+# Slice (b)
+xP <- cbind(x_grid[,1], 0.2, x2_grid[,2])
+vP <- v(xP); vP_mat <- matrix(vP, nrow=n_seq, ncol=n_seq)
+png(file="figures/TENSE-3d/v-b.png", type="cairo", width=2400, height=2400, res=600, bg="transparent")
+par(mar=c(4,4,2,2))
+filled.contour3(x=x_seq, y=x_seq, z=vP_mat, color.palette=magma, levels=seq(-1, 1, 0.05), xlab="x", ylab="z")
+dev.off()
 
-xP3 <- cbind(0.5, x2_grid[,1], x2_grid[,2]); vP3 <- v(xP3)
-vP3_mat <- matrix(vP3, nrow=n_seq, ncol=n_seq)
-filled.contour3(x=x_seq, y=x_seq, z=vP3_mat, color.palette=magma, levels=seq(-1, 1, 0.05),
-                xlab="y", ylab="z",
+# Slice (c)
+xP <- cbind(0.5, x2_grid)
+vP <- v(xP); vP_mat <- matrix(vP, nrow=n_seq, ncol=n_seq)
+png(file="figures/TENSE-3d/v-c.png", type="cairo", width=2400, height=2400, res=600, bg="transparent")
+par(mar=c(4,4,2,2))
+filled.contour3(x=x_seq, y=x_seq, z=vP_mat, color.palette=magma, levels=seq(-1, 1, 0.05), xlab="y", ylab="z",
                 plot.axes={axis(1);axis(2)
                   lines(c(0.5, 0.5), c(0, 2/3), type="l", lwd=3)
                 })
+dev.off()
 
-xP4 <- cbind(0.35, x2_grid[,1], x2_grid[,2]); vP4 <- v(xP4)
-vP4_mat <- matrix(vP4, nrow=n_seq, ncol=n_seq)
-filled.contour3(x=x_seq, y=x_seq, z=vP4_mat, color.palette=magma, levels=seq(-1, 1, 0.05),
-                xlab="y", ylab="z",
-                plot.axes={axis(1);axis(2)
-                  lines(c(0.5, 0.5), c(0, 0.6), type="l", lwd=3)
-                })
-
-xP5 <- cbind(0.3, x2_grid[,1], x2_grid[,2]); vP5 <- v(xP5)
-vP5_mat <- matrix(vP5, nrow=n_seq, ncol=n_seq)
-filled.contour3(x=x_seq, y=x_seq, z=vP5_mat, color.palette=magma, levels=seq(-1, 1, 0.05),
-                xlab="y", ylab="z",
-                plot.axes={axis(1);axis(2)
-                  lines(c(0.5, 0.5), c(0, 0.533), type="l", lwd=3)
-                })
-#dev.off()
+# Legend
+png(file="figures/TENSE-3d/v-legend.png", type="cairo", width=900, height=2400, res=600, bg="transparent")
+par(mar=c(4,0,2,2))
+colkey(col=magma(40), side=4, clim=c(-1, 1), add=F, width=4)
+dev.off()
 
 
-
-###====================================
+###=========================================
 ### True Function
-###====================================
+###=========================================
 
 f <- function(X){
   if (is.matrix(X)) { x <- X[,1]; y <- X[,2]; z <- X[,3] }
@@ -97,217 +123,217 @@ f <- function(X){
 }
 
 
-
-###====================================
 ### Plotting True Function
-###====================================
 
-
-# Cube Texture Map
-n_seq <- 201
-x_seq <- seq(0,1, l=n_seq)
-x_grid <- as.matrix(expand.grid(x_seq, x_seq))
-xPl <- list(cbind(x_grid[,1], 0, x_grid[,2]), # front
-            cbind(0, rev(x_grid[,2]), rev(x_grid[,1])), # left
-            cbind(x_grid[,1], rev(x_grid[,2]), 0), # base
-            cbind(1, rev(x_grid[,2]), x_grid[,1]), # right
-            cbind(x_grid[,1], 1, rev(x_grid[,2])), # back
-            cbind(x_grid[,1], x_grid[,2], 1)) # top
-
-png(file="figures/T3D-f-cube-texture-map.png", type="cairo", width=3*1200, height=4*1200, res=400, bg="transparent")
-par(mar=c(0,0,0,0), mfrow=c(4,3))
-for (k in c(0, 1, 0, 2, 3, 4, 0, 5, 0, 0, 6, 0)){
-  if(k==0){
-    plot(0, 0, type="n", xaxt="n", yaxt="n", xlab="", ylab="", bty="n")
-  } else {
-    xP <- xPl[[k]]
-    fP <- f(xP); fP_mat <- matrix(fP, nrow=n_seq, ncol=n_seq)
-    filled.contour3(x=x_seq, y=x_seq, z=fP_mat, color.palette=magma, levels=seq(-3.5, 3.5, 0.1), axes=F)
-  }
-}
-dev.off()
-
-
-
-# Plot Faces in Grid
-n_seq <- 201
-x_seq <- seq(0,1, l=n_seq)
+n_seq <- 101
+x_seq <- seq(0,1, l=n_seq);
 x_grid <- as.matrix(expand.grid(x_seq, x_seq))
 
+names <- c("face-front", "face-back", "face-base", "face-top", "face-left", "face-right",
+           "slice-xz1", "slice-xz2", "slice-yz")
 
-plot_f_slice <- function(xP, xlab, ylab, main=NULL) {
+xP_list <- list(cbind(x_grid[,1], 0, x_grid[,2]), cbind(x_grid[,1], 1, x_grid[,2]),
+                cbind(x_grid, 0), cbind(x_grid, 1), cbind(0, x_grid), cbind(1, x_grid),
+                cbind(x_grid[,1], 0.2, x_grid[,2]), cbind(x_grid[,1], 0.8, x_grid[,2]),
+                cbind(0.5, x_grid))
+
+mains <- c("y=0", "y=1", "z=0", "z=1", "x=0", "x=1", "y=0.2", "y=0.8", "x=0.5")
+xlabs <- c("x", "x", "x", "x", "y", "y", "x", "x", "y")
+ylabs <- c("z", "z", "y", "y", "z", "z", "z", "z", "z")
+
+disc_plots <- list(NULL, NULL,
+                   function() lines(c(1/6, 5/6), c(0.5, 0.5), type="l", lwd=2), # face-base
+                   NULL, NULL, NULL, NULL, NULL,
+                   function() lines(c(0.5, 0.5), c(0, 0.6), type="l", lwd=2)) # slice-yz
+
+
+# True Function Plots
+for (i in 1:9){
+  xP <- xP_list[[i]]
   fP <- f(xP)
   fP_mat <- matrix(fP, nrow=n_seq, ncol=n_seq)
-  filled.contour3(x=x_seq, y=x_seq, z=fP_mat, color.palette=magma, levels=seq(-3.5, 3.5, 0.1), xlab=xlab, ylab=ylab, main=main)
+  png(file=paste0("figures/TENSE-3D/f-", names[i], ".png"), type="cairo", width=2400, height=2400, res=600, bg="transparent")
+  par(mar=c(4,4,2,2), mfrow=c(1,1))
+  filled.contour3(x=x_seq, y=x_seq, z=fP_mat, color.palette=surf_cols, levels=seq(-3.5, 3.5, 0.1),
+                  xlab=xlabs[i], ylab=ylabs[i], #main=mains[i],
+                  plot.axes={axis(1);axis(2)
+                    if (!is.null(disc_plots[[i]])){
+                      disc_plots[[i]]()
+                    }
+                  })
+  dev.off()
 }
 
 
 
-png(file="figures/T3D-f-grid.png", type="cairo", width=3*1200, height=3*1200, res=400, bg="transparent")
-par(mar=c(4,4,2,2))
-par(mfrow=c(3,3))
-plot_f_slice(xP=cbind(0.5, x_grid[,1], x_grid[,2]), xlab="y", ylab="z", main="x=0.5")  # x=0.5
-plot_f_slice(xP=cbind(x_grid[,1], x_grid[,2], z=1), xlab="x", ylab="y", main="z=1")  # z=1
-plot_f_slice(xP=cbind(x_grid[,1], y=1, x_grid[,2]), xlab="x", ylab="z", main="y=1")  # y=1
+# Legend
+png(file="figures/TENSE-3d/surf-legend.png", type="cairo", width=900, height=2400, res=600, bg="transparent")
+par(mar=c(4,0,2,2))
+colkey(col=surf_cols(70), side=4, clim=c(-3.5, 3.5), add=F, width=4)
+dev.off()
 
-plot_f_slice(xP=cbind(x_grid[,1], x_grid[,2], z=0), xlab="x", ylab="y", main="z=0")  # z=0
-plot_f_slice(xP=cbind(x_grid[,1], 0.5, x_grid[,2]), xlab="x", ylab="z", main="y=0.5")  # y=0.5
-plot_f_slice(xP=cbind(x=1, x_grid[,1], x_grid[,2]), xlab="y", ylab="z", main="x=1")  # x=1
 
-plot_f_slice(xP=cbind(x_grid[,1], y=0, x_grid[,2]), xlab="x", ylab="z", main="y=0")  # y=0
-plot_f_slice(xP=cbind(x=0, x_grid[,1], x_grid[,2]), xlab="y", ylab="z", main="x=0")  # x=0
-plot_f_slice(xP=cbind(x_grid[,1], x_grid[,2], 0.5), xlab="x", ylab="y", main="z=0.5")  # z=0.5
+
+
+###=========================================
+### TENSE Emulation
+###=========================================
+#source("src/lib/TENSE.R")
+#source("src/lib/advanced_NS_emulator.R")
+
+
+### Design
+load("data/designs/Mm3_160.RData")
+xD <- Mm3_160/160 + 1/320
+colnames(xD) <- c("x", "y", "z")
+
+fD <- f(xD)
+plot_ly(x = xD[, 1], y = xD[, 2], z = xD[, 3], color = fD, type = "scatter3d", mode = "markers")
+
+pt_cexs <- list(1-abs(xD[,2]), 1-abs(xD[,2]-1),
+                1-abs(xD[,3]), 1-abs(xD[,3]-1), 1-abs(xD[,1]), 1-abs(xD[,1]-1),
+                1-abs(xD[,2]-0.2), 1-abs(xD[,2]-0.8),
+                1-abs(xD[,1]-0.5))
+
+
+#Optionally use smaller grid for emulation
+#n_seq <- 21
+#x_grid <- as.matrix(expand.grid(x_seq, x_seq))
+#x_seq <- seq(0,1, l=n_seq);
+#xP_list <- list(cbind(x_grid[,1], 0, x_grid[,2]), cbind(x_grid[,1], 1, x_grid[,2]),
+#                cbind(x_grid, 0), cbind(x_grid, 1), cbind(0, x_grid), cbind(1, x_grid),
+#                cbind(x_grid[,1], 0.2, x_grid[,2]), cbind(x_grid[,1], 0.8, x_grid[,2]),
+#                cbind(0.5, x_grid))
+
+
+em_out_list <- list()
+
+
+### Advanced Emulator
+VD <- V(xD)
+fD <- f(xD)
+vdxD <- v_deriv(xD) 
+
+# Basis Functions
+# 1, x, y, z, x^2, y^2, z^2, xz
+g <- list(function(X) 1, function(X) X[1], function(X) X[2], function(X) X[3], function(X) X[1]^2, function(X) X[2]^2, function(X) X[3]^2, function(X) X[1]*X[3])
+mu_beta <- c(-2.5, -4, 0, -1.5, 3, 2, 0, 5)
+Sigma_beta <- 10*diag(length(g))
+
+for (i in 1:9){
+  xP <- xP_list[[i]]
+  VP <- V(xP)
+  vdxP <- v_deriv(xP)
+  em_out <- advanced_NS_emulator(VxD=VD, D=fD, VxP=VP, vdxD=vdxD, vdxP=vdxP, k_S=k_S, mu_beta=mu_beta, g=g, Sigma_beta=Sigma_beta, theta=0.22, lambda2=0.06, sigma=1, just_var=T)
+  # Simple TENSE Emulator
+  #simple_NS_emulator2(VxD=VD, D=fD, VxP=VP, vdxD=vdxD, vdxP=vdxP, k_S=k_S, mu=0, theta=0.22, lambda2=0.06, sigma=1, just_var=T)
+  em_out_list[[i]] <- em_out
+  print(paste0("Completed ",i,"/9"))
+}
+
+
+
+### Regression Coefficients
+em_out_list[[1]][["ED_beta"]]
+
+
+### Emulator Plots
+for (i in 1:9){
+  em_out <- em_out_list[[i]]
+  
+  # Expectation
+  exp <- em_out[["ED_fP"]]
+  exp_mat <- matrix(exp, nrow=n_seq, ncol=n_seq)
+  
+  png(file=paste0("figures/TENSE-3D/exp-", names[i], ".png"), type="cairo", width=2400, height=2400, res=600, bg="transparent")
+  par(mar=c(4,4,2,2), mfrow=c(1,1))
+  filled.contour3(x=x_seq, y=x_seq, z=exp_mat, color.palette=surf_cols, levels=seq(-3.5, 3.5, 0.1),
+                  xlab=xlabs[i], ylab=ylabs[i], #main=mains[i],
+                  plot.axes={axis(1);axis(2)
+                    if (!is.null(disc_plots[[i]])){
+                      disc_plots[[i]]()
+                    }
+                  })
+  dev.off()
+  
+  
+  # Standard Deviation
+  var <- em_out[["VarD_fP"]] + 1e-10 # for stability
+  var_mat <- matrix(var, nrow=n_seq, ncol=n_seq)
+  sd_mat <- sqrt(var_mat)
+  
+  png(file=paste0("figures/TENSE-3D/sd-", names[i], ".png"), type="cairo", width=2400, height=2400, res=600, bg="transparent")
+  filled.contour3(x=x_seq, y=x_seq, z=sd_mat, color.palette=var_cols, levels=seq(0, 1.5, 0.05),
+                  xlab=xlabs[i], ylab=ylabs[i], #main=mains[i],
+                  plot.axes={axis(1);axis(2)
+                    if (!is.null(disc_plots[[i]])){
+                      disc_plots[[i]]()
+                    }
+                    #points(x=xD[, xlabs[i]], y=xD[, ylabs[i]], pch=21, bg=1, cex=pt_cexs[[i]])
+                  })
+  dev.off()
+  
+  
+  # Diagnostics
+  xP <- xP_list[[i]]
+  fP <- f(xP)
+  fP_mat <- matrix(fP, nrow=n_seq, ncol=n_seq)
+  diag_mat <- (exp_mat - fP_mat)/sd_mat
+  
+  png(file=paste0("figures/TENSE-3D/diag-", names[i], ".png"), type="cairo", width=2400, height=2400, res=600, bg="transparent")
+  par(mar=c(4,4,2,2), mfrow=c(1,1))
+  filled.contour3(x=x_seq, y=x_seq, z=diag_mat, color.palette=diag_cols, levels=seq(-3, 3, 0.25),
+                  xlab=xlabs[i], ylab=ylabs[i], #main=mains[i],
+                  plot.axes={axis(1);axis(2)
+                    contour(x_seq, x_seq, diag_mat, add=TRUE, level=seq(-3, 3, 0.25), lwd=0.4, drawlabels=FALSE)
+                    if (!is.null(disc_plots[[i]])){
+                      disc_plots[[i]]()
+                    }
+                  })
+  dev.off()
+}
+
+
+# Legend
+png(file="figures/TENSE-3d/var-legend.png", type="cairo", width=900, height=2400, res=600, bg="transparent")
+par(mar=c(4,0,2,2))
+colkey(col=var_cols(20), side=4, clim=c(0, 1.5), add=F, width=4)
+dev.off()
+
+# Legend
+png(file="figures/TENSE-3d/diag-legend.png", type="cairo", width=900, height=2400, res=600, bg="transparent")
+par(mar=c(4,0,2,2))
+colkey(col=diag_cols(24), side=4, clim=c(-3, 3), add=F, width=4)
 dev.off()
 
 
 
 
 
-###====================================
-### Emulation - Design
-###====================================
-
-# From space-filling-designs
-xD <- matrix(c(0, 31, 4,
-               1, 62, 9,
-               2, 13, 14,
-               3, 44, 19,
-               4, 75, 24,
-               5, 26, 29,
-               6, 57, 34,
-               7, 8, 39,
-               8, 39, 44,
-               9, 70, 49,
-               10, 21, 54,
-               11, 52, 59,
-               12, 3, 64,
-               13, 34, 69,
-               14, 65, 74,
-               15, 16, 79,
-               16, 47, 3,
-               17, 78, 8,
-               18, 29, 13,
-               19, 60, 18,
-               20, 11, 23,
-               21, 42, 28,
-               22, 73, 33,
-               23, 24, 38,
-               24, 55, 43,
-               25, 6, 48,
-               26, 37, 53,
-               27, 68, 58,
-               28, 19, 63,
-               29, 50, 68,
-               30, 1, 73,
-               31, 32, 78,
-               32, 63, 2,
-               33, 14, 7,
-               34, 45, 12,
-               35, 76, 17,
-               36, 27, 22,
-               37, 58, 27,
-               38, 9, 32,
-               39, 40, 37,
-               40, 71, 42,
-               41, 22, 47,
-               42, 53, 52,
-               43, 4, 57,
-               44, 35, 62,
-               45, 66, 67,
-               46, 17, 72,
-               47, 48, 77,
-               48, 79, 1,
-               49, 30, 6,
-               50, 61, 11,
-               51, 12, 16,
-               52, 43, 21,
-               53, 74, 26,
-               54, 25, 31,
-               55, 56, 36,
-               56, 7, 41,
-               57, 38, 46,
-               58, 69, 51,
-               59, 20, 56,
-               60, 51, 61,
-               61, 2, 66,
-               62, 33, 71,
-               63, 64, 76,
-               64, 15, 0,
-               65, 46, 5,
-               66, 77, 10,
-               67, 28, 15,
-               68, 59, 20,
-               69, 10, 25,
-               70, 41, 30,
-               71, 72, 35,
-               72, 23, 40,
-               73, 54, 45,
-               74, 5, 50,
-               75, 36, 55,
-               76, 67, 60,
-               77, 18, 65,
-               78, 49, 70,
-               79, 0, 75), nrow = 80, ncol = 3, byrow = TRUE)/80 + 1/160
-
-fD <- f(xD)
-
-
-#plot_ly(x = xD[, 1], y = xD[, 2], z = xD[, 3], color = fD, type = "scatter3d", mode = "markers")
 
 
 
-###====================================
-### Embedding Surface Derivative
-###====================================
+# Optionally can optimise choice of theta and lambda2
+#n_seq <- 21
+#x_seq <- seq(0,1, l=n_seq);
+#x2_grid <- as.matrix(expand.grid(x_seq, x_seq))
+#xP <- cbind(0.5, x2_grid[,1], x2_grid[,2])
 
+#VD <- V(xD); VP <- V(xP)
+#fD <- f(xD)
+#vdxD <- v_deriv(xD); vdxP <- v_deriv(xP)
+#fP <- f(xP)
 
-s_deriv <- function(d) {
-  (4*d * (0 <= d & d < 0.5)) + (-4*(d-1) * (0.5 <= d & d < 1))
-}
+#wrapper <- function(params) {
+#  theta <- params[1]
+#  lambda2 <- abs(params[2])
+#  
+#  exp <- simple_NS_emulator2(VxD=VD, D=fD, VxP=VP, vdxD=vdxD, vdxP=vdxP, k_S=k_S, mu=0, theta=theta, lambda2=lambda2, sigma=1, just_var=T)[["ED_fP"]]
+#  return(mean((exp-fP)^2)) # MSE
+#}
 
-v_deriv <- function(X){
-  if (is.matrix(X)) { x <- X[,1]; y <- X[,2]; z <- X[,3] }
-  else { x <- X[1]; y <- X[2]; z <- X[3] }
-  
-  #A <- 1 - s(3*sqrt((x-0.5)^2 + (0.5*z-0)^2 + 1e-6))
-  dxA <- -s_deriv(3*sqrt((x-0.5)^2 + (0.5*z-0)^2)) * 3/(2*sqrt((x-0.5)^2 + (0.5*z-0)^2 + 1e-6)) * 2*(x-0.5)
-  dyA <- rep(0, l=length(y))
-  dzA <- -s_deriv(3*sqrt((x-0.5)^2 + (0.5*z-0)^2)) * 3/(2*sqrt((x-0.5)^2 + (0.5*z-0)^2 + 1e-6)) * 0.5*z
-  
-  B <- 1 - 2*(y > 0.5)
-  
-  dx <- dxA*B
-  dy <- dyA*B
-  dz <- dzA*B
-  return(mapply(c, dx, dy, dz, SIMPLIFY=F))
-}
-
-
-# Design points fD, vD and derivatives
-#vD <- v(xD)
-#vD_deriv <- do.call(rbind, v_deriv(xD))
-
-#plot_ly(x = xD[, 1], y = xD[, 2], z = xD[, 3], color = vD, type = "scatter3d", mode = "markers")
-#plot_ly(x = xD[, 1], y = xD[, 2], z = xD[, 3], color = vD_deriv[,1], type = "scatter3d", mode = "markers")
-#plot_ly(x = xD[, 1], y = xD[, 2], z = xD[, 3], color = vD_deriv[,2], type = "scatter3d", mode = "markers")
-#plot_ly(x = xD[, 1], y = xD[, 2], z = xD[, 3], color = vD_deriv[,3], type = "scatter3d", mode = "markers")
-
-
-
-
-
-###====================================
-### TENSE
-###====================================
-
-#source("src/lib/TENSE.R")
-
-
-
-CmatD <- k_NS(VX=V(xD), vdX=v_deriv(xD), k_S=k_S, theta=0.3, lambda2=1, sigma=1)
-
-
-
+#result <- optim(par=c(0.3, 0.1), fn=wrapper, method="L-BFGS-B") #"L-BFGS-B" or "Nelder-Mead" (simplex method)
+#result$par
 
 
 
